2c7c3a8eb6dd395cbb36e1c1cbade530f40ef473
Giani2110 <gianfranco.andreachi@davinci.edu.ar>
2024-10-26 16:41:28 -0300

    Agregar CategoryController CRUD

--- backend/Controller/CategoryController.py
+++ backend/Controller/CategoryController.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
-from backend.security.auth import token_required
+from flask import Blueprint, jsonify, request
+from backend.controller.UserController import save_data
+from backend.security.auth import token_required, has_any_role
 import json
 from backend.config.config import DATABASE_FILE, API_PREFIX
 
@@ -12,3 +13,56 @@ def get_categories():
     data = json.load(db_file)
     categories = data.get('categories', [])
     return jsonify(categories), 200
+  
+@category_controller.route('/categories', methods=['POST'])
+@token_required
+@has_any_role(['ADMIN'])
+def create_category():
+    with open(DATABASE_FILE) as db_file:
+        data = json.load(db_file)
+        categories = data.get('categories', [])
+        new_category = {'id': len(categories) + 1}
+        new_category['name'] = request.json.get('name')
+        categories.append(new_category)
+        data['categories'] = categories
+        save_data(data)
+        return jsonify({"message": "Categoría creada exitosamente"}), 201
+
+@category_controller.route('/categories/<int:id>', methods=['DELETE'])
+@token_required
+@has_any_role(['ADMIN'])
+def delete_category(id):
+  with open(DATABASE_FILE) as db_file:
+    data = json.load(db_file)
+    categories = data.get('categories', [])
+    for category in categories:
+      if category['id'] == id:
+        categories.remove(category)
+        save_data(data)
+        return jsonify({"message": "Categoría eliminada exitosamente"}), 200
+    return jsonify({"message": "Categoría no encontrada"}), 404
+
+@category_controller.route('/categories/<int:id>', methods=['PUT'])
+@token_required
+@has_any_role(['ADMIN'])
+def update_category(id):
+  with open(DATABASE_FILE) as db_file:
+    data = json.load(db_file)
+    categories = data.get('categories', [])
+    for category in categories:
+      if category['id'] == id:
+        category['name'] = request.json['name']
+        save_data(data)
+        return jsonify({"message": "Categoría actualizada exitosamente"}), 200
+    return jsonify({"message": "Categoría no encontrada"}), 404
+  
+@category_controller.route('/categories/<int:id>', methods=['GET'])
+@token_required
+def get_category(id):
+  with open(DATABASE_FILE) as db_file:
+    data = json.load(db_file)
+    categories = data.get('categories', [])
+    for category in categories:
+      if category['id'] == id:
+        return jsonify(category), 200
+    return jsonify({"message": "Categoría no encontrada"}), 404
